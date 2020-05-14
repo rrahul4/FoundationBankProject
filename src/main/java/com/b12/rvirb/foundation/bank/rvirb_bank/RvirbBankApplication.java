f56c687fbb7b575f2aeec813df68cdeed9ac6684
Rahulkumar Rakhonde <rahul.rakhonde@gmail.com>
2020-05-14 17:27:31 +0530

    First Commit

---
+++ src/main/java/com/b12/rvirb/foundation/bank/rvirb_bank/RvirbBankApplication.java
@@ -0,0 +1,13 @@
+package com.b12.rvirb.foundation.bank.rvirb_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RvirbBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RvirbBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/b12/rvirb/foundation/bank/rvirb_bank/RvirbBankApplicationTests.java
@@ -0,0 +1,14 @@
+package com.b12.rvirb.foundation.bank.rvirb_bank;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RvirbBankApplicationTests {
+
+/*	@Test
+	void contextLoads() {
+	}*/
+
+}
